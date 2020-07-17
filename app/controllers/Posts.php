9909f25624122195da8391d467e8cfba8706ade5
HojinChuu <cnghwls7@gmail.com>
2020-07-17 15:28:32 +0900

    create post

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -24,11 +24,46 @@ class Posts extends Controller
 
     public function add()
     {
-        $data = [
-            'title' => '',
-            'body' => ''
-        ];
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-        $this->view('posts/add', $data);
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $data = [
+                'title' => trim($_POST['title']),
+                'body' => trim($_POST['body']),
+                'user_id' => $_SESSION['user_id'],
+                'title_err' => '',
+                'body_err' => ''
+            ];
+
+            if(empty($data['title'])) {
+                $data['title_err'] = "제목을 입력해주세요";
+            }
+
+            if(empty($data['body'])) {
+                $data['body_err'] = "내용을 입력해주세요";
+            }
+
+            if(empty($data['title_err']) && empty($data['body_err'])) {
+                
+                if($this->postModel->addPost($data)) {
+                    flash('post_message', 'Post Added');
+                    redirect('posts/index');
+                } else {
+                    die('Something went wrong');
+                }
+
+            } else {
+                $this->view('posts/add', $data);
+            }
+
+        } else {
+            $data = [
+                'title' => '',
+                'body' => ''
+            ];
+    
+            $this->view('posts/add', $data);
+        }
     }
 }
